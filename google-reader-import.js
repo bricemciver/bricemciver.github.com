880de18396d340cee3213f2f684bd39fdede70d5
bricemciver <github@bricemciver.com>
2013-05-20 14:12:33 -0500

    Update google-reader-import.js

--- google-reader-import.js
+++ google-reader-import.js
@@ -3,15 +3,15 @@ function createStoryEntry(jsonItem) {
   var content = (typeof jsonItem.content === 'undefined') ? jsonItem.summary.content : jsonItem.content.content;
   var entryMain = document.createElement("div");
   entryMain.className = "entry-main";
-  entryMain.innerHTML = '&lt;div class="entry-date"&gt;' + publishedDate.toDateString() + ' ' + 
-      publishedDate.toTimeString() + '&lt;/div&gt;&lt;h2 class="entry-title"&gt;' + 
-      '&lt;a class="entry-title-link" href="' + jsonItem.alternate.href + '" target="_blank"&gt;' + 
-      jsonItem.title + '&lt;/a&gt;&lt;/h2&gt;&lt;div class="entry-author"&gt;' + 
-      '&lt;span class="entry-source-title-parent"&gt;from &lt;a class="entry-source-title" href="' + 
-      jsonItem.origin.htmlUrl + '" target="_blank"&gt;' + jsonItem.origin.title + 
-      '&lt;/a&gt;&lt;/span&gt;&lt;/div&gt;&lt;div class="entry-body"&gt;&lt;div&gt;' +
-      '&lt;div class="item-body"&gt;&lt;div&gt;' + content + '&lt;/div&gt;&lt;/div&gt;' +
-      '&lt;/div&gt;&lt;/div&gt;';
+  entryMain.innerHTML = '<div class="entry-date">' + publishedDate.toDateString() + ' ' + 
+      publishedDate.toTimeString() + '</div><h2 class="entry-title">' + 
+      '<a class="entry-title-link" href="' + jsonItem.alternate.href + '" target="_blank">' + 
+      jsonItem.title + '</a></h2><div class="entry-author">' + 
+      '<span class="entry-source-title-parent">from <a class="entry-source-title" href="' + 
+      jsonItem.origin.htmlUrl + '" target="_blank">' + jsonItem.origin.title + 
+      '</a></span></div><div class="entry-body"><div>' +
+      '<div class="item-body"><div>' + content + '</div></div>' +
+      '</div></div>';
   return entryMain;
 }
 function handleFileSelect() {
@@ -20,7 +20,7 @@ function handleFileSelect() {
   var reader = new FileReader();
   reader.onload = function(evt) {
     var parsedStories = JSON.parse(evt.target.result);
-    for (var i = 0; i &lt; parsedStories.items.length; i++) {
+    for (var i = 0; i < parsedStories.items.length; i++) {
       storiesContainer.appendChild(createStoryEntry(parsedStories.items[i]));
     }
   };
